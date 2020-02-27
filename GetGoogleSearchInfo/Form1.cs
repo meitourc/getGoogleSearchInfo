cba8df9694735d4523da88248564b4901aecdbe5
Takuma Yamada <meitourc@gmail.com>
2020-02-27 19:36:21 +0900

    htlm取得処理作成途中

--- GetGoogleSearchInfo/Form1.cs
+++ GetGoogleSearchInfo/Form1.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -29,8 +32,40 @@ namespace GetGoogleSearchInfo
         /// <param name="e"></param>
         private void button_getData_Click(object sender, EventArgs e)
         {
+            WebClient wc = new WebClient();
+            Stream st = wc.OpenRead("https://www.google.co.jp/search?q=テスト&ie=UTF-8&oe=UTF-8&num=20");
 
+            Encoding enc = Encoding.GetEncoding("UTF-8");
+            StreamReader sr = new StreamReader(st, enc);
+            string html = sr.ReadToEnd();
+            scrapingData(html);
+
+            st.Close();
+
+        }
+
+
+        private void scrapingData(string html)
+        {
+
+            //string html = getItemPictureHtml(asinUrl);
+            html = html.Replace("\r", "").Replace("\n", "");
+
+            string pattern = "";
+            pattern = "imgTagWrapperId(.*)</h3";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(html);
+
+
+            foreach (var item in match.Groups)
+            {
+                Console.WriteLine("\n-------------------------------------------------------------------\n");
+                Console.WriteLine(item);
+
+
+            }
 
         }
     }
 }
+
